2b6b4eaca75d8abde8803f49bf536cfe3bbc5bbd
Sebahubaide <shubaide@gmail.com>
2022-05-19 20:04:30 -0300

    Update Imagen.java

--- src/main/java/com/All4pets/All4pets/entidades/Imagen.java
+++ src/main/java/com/All4pets/All4pets/entidades/Imagen.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.All4pets.All4pets.entidades;
 
-/**
- *
- * @author Sebastian Hubaide
- */
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Imagen {
     
+    @Id
+    private String id;
+    private String nombre;
+    private
 }
